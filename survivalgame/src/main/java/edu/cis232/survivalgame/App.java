c73a8af3e7d143234464ffb1dc7eb62a758741f1
Kevin Cressman <original7nationarmy@gmail.com>
2016-04-20 19:45:58 -0400

    started a skeleton of the player's interaction with the environment. will have 2 object arrays, where it can track the state of doors and interactive things on the map, and picked up and usable items in inventory.

--- survivalgame/src/main/java/edu/cis232/survivalgame/App.java
+++ survivalgame/src/main/java/edu/cis232/survivalgame/App.java
@@ -9,5 +9,23 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Object[] map = { new Object("Door 1") };
+        Object[] inventory = { new Object("Key 1")};
+		
+		for(Object object : map){
+			OpenifDoor(object);
+		}
     }
-}
+    
+		public static void OpenifDoor(Object object){
+			if(object instanceof Door ||object instanceof Chest){
+				Openable opened = (Openable)object;
+				opened.Open();
+			} else {
+				System.out.println(object + " is not openable");
+			}
+		}
+
+    }
+    
+    
