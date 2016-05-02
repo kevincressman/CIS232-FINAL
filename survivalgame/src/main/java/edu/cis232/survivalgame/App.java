3ee34f813e69d39ea63cdbcc900a893bc1a97a8b
Kevin Cressman <original7nationarmy@gmail.com>
2016-05-02 17:40:54 -0400

    ONE FORKTO RULE THEM ALL

--- survivalgame/src/main/java/edu/cis232/survivalgame/App.java
+++ survivalgame/src/main/java/edu/cis232/survivalgame/App.java
@@ -1,24 +1,42 @@
 package edu.cis232.survivalgame;
 import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        Openable[] map = { new Door("First Door", 0), new LockedDoor("Iron Door", 1), new Chest("Wooden Chest", new Item ("Second Key", 2)), new LockedDoor("Portcullis", 2) };
-        //{ new Item("First Key", 1)};
-		ArrayList<Item> inventory = new ArrayList<Item>();
-		inventory.add(new Item("First Key", 1));
-		for(Object object : map){
-			//on click
-			OpenifOpenable(object, inventory);
-		}
-    }
+public class App extends Application
+{    public static void main( String[] args ){
+    System.out.println( "Hello World!" );
+    launch(args);
+}
+//        System.out.println( "Hello World!" );
+//        Openable[] map = { new Door("First Door", 0), new LockedDoor("Iron Door", 1), new Chest("Wooden Chest", new Item ("Second Key", 2)), new LockedDoor("Portcullis", 2) };
+//        //{ new Item("First Key", 1)};
+//		ArrayList<Item> inventory = new ArrayList<Item>();
+//		inventory.add(new Item("First Key", 1));
+//		for(Object object : map){
+//			//on click
+//			OpenifOpenable(object, inventory);
+//}}
+		
     
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		Parent parent = FXMLLoader.load(App.class.getResource("Game.fxml"));
+		
+		Scene scene = new Scene(parent);
+		
+		stage.setScene(scene);
+		//REQ#1
+		stage.setTitle("Paul and Kevin's Survival Game!");	
+		stage.show();
+	}
 		public static void OpenifOpenable(Object object, ArrayList<Item> inventory){
 			if(object instanceof Door){
 				Openable opened = (Openable)object;
---
+++ survivalgame/src/main/java/edu/cis232/survivalgame/CreateImageDB.java
@@ -0,0 +1,88 @@
+package edu.cis232.survivalgame;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateImageDB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			initDB();
+    	}
+	
+
+	/*
+	 * Drops Image Table if already exists
+	 */
+	public static void dropTables(Connection conn) {
+		System.out.println("Checking for existing tables.");
+	
+		try {
+				Statement stmt = conn.createStatement();
+			
+			try {
+				
+				stmt.execute("DROP TABLE Image");
+				System.out.println("Image table dropped.");
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+			
+		} catch (SQLException ex) {
+			System.out.println("ERROR: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	
+	/*
+	 * Creates the Image Table
+	 * also adds in four rows of data
+	 */
+    
+	public static void buildImageTable(Connection conn) {
+		try {
+
+			Statement stmt = conn.createStatement();
+
+			// Create the table.
+			stmt.execute("CREATE TABLE Image (" + "ImageID CHAR(10) NOT NULL PRIMARY KEY, " + "Name CHAR(25), " + "Address CHAR(5000) " + ")");
+
+
+			// Insert row #1.Door
+			stmt.execute("INSERT INTO Image VALUES ( " + "'D-101', " + "'Door_1', " 
+						 + "'http://1.bp.blogspot.com/_5ke3OeOEo0g/TLrm7groP8I/AAAAAAAACqU/6FQEcnYbMXY/s1600/3_door.jpg' )");
+
+			// Insert row #2.Hall
+			stmt.execute("INSERT INTO Image VALUES ( " + "'H-101', " + "'Hall_1', " 
+						 + "'https://s-media-cache-ak0.pinimg.com/736x/ed/40/aa/ed40aa1a34995ce14b0a7e57dc3f3b8c.jpg' )");
+			
+			//Insert row #3.Chest
+			stmt.execute("INSERT INTO Image VALUES ( " + "'C-101', " + "'Chest_1', " 
+					 + "'https://revphil2011.files.wordpress.com/2011/04/treasure_chest_medium.png' )");
+			
+
+			System.out.println("Image table created.");
+		} catch (SQLException ex) {
+			System.out.println("ERROR: " + ex.getMessage());
+		}
+	}
+	
+	public static void initDB(){
+		final String DB_URL = "jdbc:hsqldb:file:ObjectsDB/objects;hsqldb.lock_file=false";
+
+		try {
+			// Create a connection to the database.
+			Connection conn = DriverManager.getConnection(DB_URL);
+
+			dropTables(conn);
+			
+			buildImageTable(conn);
+			
+			conn.close();
+		} catch (Exception ex) {
+			System.out.println("ERROR: " + ex.getMessage());
+		}
+	}
+}
