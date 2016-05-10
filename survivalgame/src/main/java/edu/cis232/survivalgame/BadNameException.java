5cc34464014f2eae154c65c6169005a9740b5d23
Paul Turnbaugh <pet30200@gmail.com>
2016-05-09 20:06:12 -0400

    Update

--- survivalgame/src/main/java/edu/cis232/survivalgame/BadNameException.java
+++ survivalgame/src/main/java/edu/cis232/survivalgame/BadNameException.java
@@ -3,7 +3,7 @@ package edu.cis232.survivalgame;
 public class BadNameException extends Exception{
 
 	public BadNameException(String name) {
-			super("Please Enter a Name");
+			super(String.format("Please Enter a Name"));
 	}
 
 }
